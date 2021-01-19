dc5a48de832bd3c934299c3fbd60af9665e25e8d
Nursultan <nabakirov@gmail.com>
2021-01-19 19:56:17 +0600

    fix verbose name of user in category model

--- category/models.py
+++ category/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
         verbose_name = _('category')
         verbose_name_plural = _('categories')
     title = models.CharField(_('title'), max_length=200, null=False)
-    user = models.ForeignKey('user.User', on_delete=models.CASCADE, null=False, verbose_name=_('category'))
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE, null=False, verbose_name=_('user'))
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, related_name='children', blank=True,
                                verbose_name=_('parent'))
     creation_date = models.DateTimeField(_('creation date'), null=False, auto_now_add=True)
