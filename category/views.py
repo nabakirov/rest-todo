3fdf615da66a9cda8afbb739dd8feabd9eb82a27
Nursultan <nabakirov@gmail.com>
2021-01-20 20:00:27 +0600

    add tree view for category api

--- category/views.py
+++ category/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from . import models, serializers as s
 from rest_todo.mixins import FilterObjectByUserMixin, FilterObjectByPK
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class CategoryViewSet(FilterObjectByUserMixin,
@@ -15,3 +17,20 @@ class CategoryViewSet(FilterObjectByUserMixin,
 
     def get_queryset(self):
         return models.Category.objects
+
+    def _tree_representation(self, parent_id, queryset):
+        data = {}
+        children = []
+        for category in queryset:
+            if category.id == parent_id:
+                data = self.get_serializer(instance=category).data
+            elif category.parent_id == parent_id:
+                children.append(self._tree_representation(category.id, queryset))
+        data['children'] = children
+        return data
+
+    @action(methods=('GET',), detail=False, url_path='tree')
+    def tree(self, request):
+        queryset = self.get_queryset().filter(user=self.request.user).all()
+        data = self._tree_representation(None, queryset)
+        return Response(data['children'])
