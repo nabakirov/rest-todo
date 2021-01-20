ec702edbfb716e3696103aea47a8efa29ede1419
Nursultan <nabakirov@gmail.com>
2021-01-20 12:42:22 +0600

    apply mixins to category api

--- category/views.py
+++ category/views.py
@@ -1,14 +1,17 @@
 from rest_framework import viewsets
 from . import models, serializers as s
+from rest_todo.mixins import FilterObjectByUserMixin, FilterObjectByPK
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(FilterObjectByUserMixin,
+                      FilterObjectByPK,
+                      viewsets.ModelViewSet):
     serializer_class = s.CategorySerializer
-    filterset_fields = ('parent_id',)
     search_fields = ('title',)
+    filter_pk_field = 'parent_id'
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
     def get_queryset(self):
-        return models.Category.objects.filter(user=self.request.user).all()
+        return models.Category.objects
