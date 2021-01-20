abecde87c5b0055cecab9df13c35cafde886a055
Nursultan <nabakirov@gmail.com>
2021-01-20 12:42:31 +0600

    apply mixins to todo api

--- todo/views.py
+++ todo/views.py
@@ -1,18 +1,20 @@
 from rest_framework import viewsets
 from . import models, serializers as s
-from rest_todo.mixins import ActionSerializerClassMixin, FilterObjectByUserMixin
+from rest_todo.mixins import ActionSerializerClassMixin, FilterObjectByUserMixin, FilterObjectByPK
 
 
 class TodoViewSet(ActionSerializerClassMixin,
                   FilterObjectByUserMixin,
+                  FilterObjectByPK,
                   viewsets.ModelViewSet):
     action_serializer_class = {
         'list': s.ShortTodoSerializer,
         'retrieve': s.TodoSerializer,
     }
     serializer_class = s.TodoRequestSerializer
-    filterset_fields = ('category_id', 'schedule', 'is_done')
+    filterset_fields = ('schedule', 'is_done')
     search_fields = ('title', 'description')
+    filter_pk_field = 'category_id'
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
